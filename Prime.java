448b5cf29161505656968ff38e55c5fcf2a6570c
= <=>
2024-12-22 16:54:35 +0530

    first 100 prime numbers

---
+++ Prime.java
@@ -0,0 +1,10 @@
+public class Prime{
+    public static void main(String[] args) {
+        int n = 200;
+        for(int i = 1;i<=n;i++){
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }q
+}
